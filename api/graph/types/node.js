29aa07a70102285ce287e6f18a0c5fb40f84ebf7
Mark Potter <si3rr4wow@gmail.com>
2023-10-17 15:06:48 +0100

    less complicated type resolution

--- api/graph/types/node.js
+++ api/graph/types/node.js
@@ -1,37 +1,28 @@
-const { nodeDefinitions, fromGlobalId, toGlobalId } = require('graphql-relay');
+const { nodeDefinitions, fromGlobalId } = require('graphql-relay');
 const { getComment, getPost, getUser, getImage } = require('../resolvers');
 const { idStubs } = require('./id-stubs');
 
-// Attach a _guid field during node resolution,
-// we can use this to discriminate node types
-// later by decoding type and id from the guid
-const injectGuid = (resolver, idStub) => (id) =>
+const injectType = (resolver, type) => (id) =>
   new Promise((resolve, reject) => {
     resolver(id).then((response) => {
-      if (!response.id)
+      if (response._type)
         reject(
           new Error(
-            "Could not inject a _guid on the specified object, the resolved object didn't have an id."
-          )
-        );
-      if (response._guid)
-        reject(
-          new Error(
-            'The resolved object already had a _guid field specified, avoid storing this in data.'
+            'The resolved object already had a _type field specified, avoid storing this in data.'
           )
         );
       resolve({
         ...response,
-        _guid: toGlobalId(idStub, id),
+        _type: type,
       });
     }, reject);
   });
 
 const resolvers = {
-  [idStubs.Comment]: injectGuid((id) => getComment(id), 'Comment'),
-  [idStubs.Post]: injectGuid((id) => getPost(id), 'Post'),
-  [idStubs.Image]: injectGuid((id) => getImage(id), 'Image'),
-  [idStubs.User]: injectGuid((id) => getUser(id), 'User'),
+  [idStubs.Comment]: injectType((id) => getComment(id), 'Comment'),
+  [idStubs.Post]: injectType((id) => getPost(id), 'Post'),
+  [idStubs.Image]: injectType((id) => getImage(id), 'Image'),
+  [idStubs.User]: injectType((id) => getUser(id), 'User'),
 };
 
 const fetchByGuid = (guid) => {
@@ -51,9 +42,8 @@ const discriminateType = (obj) => {
     [idStubs.User]: User,
   };
 
-  // N.B. _guid is attached during resolution, it's not in the data layer
-  const { type } = fromGlobalId(obj._guid);
-  return typeMap[type];
+  // N.B. _type is attached during resolution, it's not in the data layer
+  return typeMap[obj._type];
 };
 
 const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
